db83a6c8ad78aa1b818d72d68a82e30875580d4b
EpicHorrors <137189551+EpicHorrors@users.noreply.github.com>
2024-08-03 14:34:01 +0700

    Add files via upload

---
+++ PhantomPlus/Patches/PingTrackerPatch.cs
@@ -0,0 +1,45 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using TMPro;
+using System.Text;
+
+namespace PhantomPlus.Patches;
+
+[HarmonyPatch(typeof(PingTracker), nameof(PingTracker.Update))]
+public class CreditsPatch
+{
+
+
+
+    public static void Postfix(PingTracker __instance)
+    {
+        var position = __instance.GetComponent<AspectPosition>();
+        position.DistanceFromEdge = new Vector3(0f, 5.8544f, 0);
+        
+        position.AdjustPosition();
+        var host = GameData.Instance?.GetHost();
+
+        
+
+        __instance.text.outlineColor = Color.black;
+
+        __instance.text.outlineWidth = 0.4f;
+
+        __instance.text.alignment = TextAlignmentOptions.Center;
+
+        __instance.text.text += $"<size=130%><color=#00FFFF>\nNotEnoughFeature</color> <color=#FFFFFF>v1.1</color>";
+
+
+        __instance.text.text += $"<size=60%>\nModded by <color=#FF0000>EpicHorrors</color>, <color=#A020F0>TheLegend</color> & <color=#808080>Techiee</color> \nDesign by <color=#FF0000>EpicHorrors</color> <color=#FFFF00>\nThanks to all modders that help me</color>";
+
+
+
+
+        
+
+
+    }
+
+
+
+}
--- PhantomPlus/Properties/Resources.Designer.cs
+++ PhantomPlus/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace PhantomPlus.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] noclipmod {
-            get {
-                object obj = ResourceManager.GetObject("noclipmod", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
     }
 }
